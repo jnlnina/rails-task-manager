8956cbf9d25de770f66d71649b13008dd8c1e603
Janina Lodeiro <nina.lodeiro@gmail.com>
2023-02-22 07:27:39 +0000

    updated controller along with views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List all tasks
+  # root to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  # Show create form
+  get 'tasks/new', to: 'tasks#new'
+  # Create the task
+  post 'tasks', to: 'tasks#create'
+  # List one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Show edit form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # Update task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Delete task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
